ff615122d0cdab22d80cbc4d6dfb4917036590fc
MakoskiVictor <makoski.ed@gmail.com>
2023-06-10 19:06:43 -0300

    added fill the database

--- Api/app/Http/Controllers/CountriesController.php
+++ Api/app/Http/Controllers/CountriesController.php
@@ -6,6 +6,7 @@ use App\Models\countries;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StorecountriesRequest;
 use App\Http\Requests\UpdatecountriesRequest;
+use Ramsey\Uuid\Uuid;
 
 class CountriesController extends Controller
 {
@@ -14,7 +15,60 @@ class CountriesController extends Controller
      */
     public function index()
     {
-        //
+        // Para recuperar todos los registros de la tabla countries
+        if (countries::count() === 0) {
+            // Lamada a api para llenar la database
+            $url = "https://restcountries.com/v3.1/all";
+
+            // CONFIGURO LAS OPCIONES DE LECTURA
+            $options = array("ssl" => array("verify_peer" => false, "verify_prr_name" => false));
+
+            // FILE_GET_CONTENTE ME PERMITE LEER EL CONTENIDO DE ACUERDO A MIS OPCIONES CONFIGURADAS
+            $response = file_get_contents($url, false, stream_context_create($options));
+
+            $objResponse = json_decode($response);
+
+
+            $filteredCountries = array_map(function($c) {
+                return (object) [
+                "name" => property_exists($c, 'name') ? $c -> name -> common : $c -> name -> official,
+                "capital" => property_exists($c, 'capital') ? $c -> capital[0] : "No info",
+                "region" => property_exists($c, 'region') ? $c -> region : "No info",
+                "subregion" => property_exists($c, 'subregion') ? $c -> subregion : "No info",
+                "languages" => property_exists($c, 'languages') ? reset($c -> languages) : "No info",
+                "maps" => property_exists($c, 'maps') ? $c -> maps -> googleMaps : "No info",
+                "population" => property_exists($c, 'population') ? $c -> population : 0,
+                "flag" => property_exists($c, 'flags') ? $c -> flags -> svg : $c -> flags -> png,
+                "area" => property_exists($c, 'area') ? $c -> area : 0
+                ];
+
+            
+            }, $objResponse);
+
+            // LLENAR LA DATABASE CON NUEVA INFO
+            foreach ($filteredCountries as $fc) {
+                $uuid = Uuid::uuid4()->toString();
+                
+                $country = new countries();
+                $country->id = $uuid;
+                $country->name = $fc->name;
+                $country->capital = $fc->capital;
+                $country->region = $fc->region;
+                $country->subregion = $fc->subregion;
+                $country->languages = $fc->languages;
+                $country->maps = $fc->maps;
+                $country->population = $fc->population;
+                $country->flag = $fc->flag;
+                $country->area = $fc->area;
+                $country->save();
+            }
+            
+            return response()->json(['message' => 'La base de datos se ha llenado correctamente']);
+
+
+        } else {
+            return countries::All();
+        }
     }
 
     /**
