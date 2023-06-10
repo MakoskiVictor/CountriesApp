4766c2d5539fefe92694239d44e1261d4cf4050b
MakoskiVictor <makoski.ed@gmail.com>
2023-06-10 19:05:40 -0300

    added countries route

--- Api/routes/api.php
+++ Api/routes/api.php
@@ -2,18 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CountriesController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// ACÁ DEFINIMOS LAS RUTAS PARA LA API
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::apiResource('countries', CountriesController::class);
+
+
+
+
+
+
+
+
+
+
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+}); */
