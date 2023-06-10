852f4a64fb4b0562338513f5618b0b266b345348
MakoskiVictor <makoski.ed@gmail.com>
2023-06-10 19:06:08 -0300

    added primary id

--- Api/database/migrations/2023_06_09_181043_create_countries_table.php
+++ Api/database/migrations/2023_06_09_181043_create_countries_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('countries', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->string('capital');
             $table->string('region');
