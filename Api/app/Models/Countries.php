ab493d6b3b78b1bda787629455d76ab8b95fdad7
MakoskiVictor <makoski.ed@gmail.com>
2023-06-12 16:57:29 -0300

    added params for uuid

--- Api/app/Models/Countries.php
+++ Api/app/Models/Countries.php
@@ -8,6 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Countries extends Model
 {
     use HasFactory;
+    // Linea 12, 13 y 14 son importantes para la generacion de uuid
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+    protected $keyType = 'string';
 
     // Relaciones de las tablas
     public function activities () {
